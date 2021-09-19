303e893b939afab2b7495c4bcd482d02c2f168ac
nebr0k <kocubaostap1@gmail.com>
2021-09-19 20:30:55 +0300

    commit2

--- 2.1/2.1.cpp
+++ 2.1/2.1.cpp
@@ -12,13 +12,13 @@ int main()
 {
 	double m; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
-	double z2; // результат обчислення 2-го виразу
+	//double z2; // результат обчислення 2-го виразу
 	cout << "m = "; cin >> m;
 	z1 = (sin(m) + sin(5 * m) - sin(3 * m)) / (cos(m) - cos(3 * m) + cos(5 * m));
-	z2 = tan(3*m);
+	//z2 = tan(3*m);
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	//cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
